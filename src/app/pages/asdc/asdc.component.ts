10733828e6bc14a1be50042a63615ad0385a29dc
Sebastian Silva <casesime2.1@gmail.com>
2020-05-24 11:09:50 -0500

    nuevo inicio x3

--- src/app/pages/asdc/asdc.component.ts
+++ src/app/pages/asdc/asdc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-asdc',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsdcComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) { 
+    config.interval = 5000;
+    config.wrap = true;
+    config.keyboard = false;
+    config.showNavigationArrows = true;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
