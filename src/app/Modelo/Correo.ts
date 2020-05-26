fe963ff5a504db9900e130f4021452b0b876a17d
Miguel <miguelfaob@gmail.com>
2020-05-26 14:45:00 -0500

    Se agrego envio de correos

---
+++ src/app/Modelo/Correo.ts
@@ -0,0 +1,13 @@
+export class Correo{
+    nombre: Number;
+    correo:String;
+    asunto:number;
+    cuerpo:String;
+
+    constructor(){
+        this.nombre=this.nombre;
+        this.correo=this.correo;
+        this.asunto=this.asunto;
+        this.cuerpo=this.cuerpo;
+    }
+}
--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Correo } from '../../Modelo/Correo';
+import { CorreosService } from '../../servicios/correos.service';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-contacto',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit {
-
-  constructor() { }
+  formularioCorreo:FormGroup;
+  CorreoTempo: Correo= new Correo();
+  mensaje: String = new String();
+  constructor(private formulario:FormBuilder, public envioCorreos:CorreosService ) { }
 
   ngOnInit(): void {
+    this.formularioCorreo=this.formulario.group({
+      nombre:['',Validators.required],
+      asunto:['',Validators.required],
+      correo:['',Validators.compose([
+        Validators.required,Validators.email    
+      ])],  
+      cuerpo:['',Validators.required]
+    });
+  }
+
+  enviarCorreo(){
+    if(this.formularioCorreo.valid){
+      this.CorreoTempo=this.formularioCorreo.value as Correo
+
+      this.envioCorreos.enviarCorreo(this.CorreoTempo).subscribe((String)=>{
+        console.log("Se envio correo ")
+        this.mensaje="Se mando el correo"
+      });
+      
+    }
   }
 
 }
