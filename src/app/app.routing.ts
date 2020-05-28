f7bd82b47d4e2d5240b24e3e8e99d3123224e2a2
David Hidalgo <davidhiga@unisabana.edu.co>
2020-05-28 14:29:30 -0500

    aaaa

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
 import { TiendaComponent } from './pages/tienda/tienda.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { DesayunoComponent } from './pages/desayuno/desayuno.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NosotrosComponent } from './pages/nosotros/nosotros.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
@@ -23,6 +25,8 @@ const routes: Routes =[
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: InicioComponent },
     { path: 'tienda', component: TiendaComponent },
+    { path: 'producto', component: ProductoComponent },
+    { path: 'desayuno', component: DesayunoComponent },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'nosotros', component: NosotrosComponent },
