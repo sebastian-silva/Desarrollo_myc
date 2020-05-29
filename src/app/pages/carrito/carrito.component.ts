b2d9757cff652da9999d266efadf9812fcf11d53
Imbachi <nicolas.imbachi.n@gmail.com>
2020-05-29 11:03:44 -0500

    No carga el boton

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-carrito',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
+
+  
 
   ngOnInit(): void {
   }
