a32291381a7894a7a4e380e6ece1fd6812510b4e
David Hidalgo <davidhiga@unisabana.edu.co>
2020-05-29 14:55:25 -0500

    alerta desayuno

--- src/app/pages/desayuno/desayuno.component.ts
+++ src/app/pages/desayuno/desayuno.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-desayuno',
   templateUrl: './desayuno.component.html',
@@ -11,5 +11,13 @@ export class DesayunoComponent implements OnInit {
   constructor(private ruta: Router) { }
 
   ngOnInit(): void {
+  }
+  showModal(){
+    Swal.fire(
+      '¡Buen provecho!',
+      'El producto se ha agregado al carrito',
+      'success'
+    )
+
   }
 }
