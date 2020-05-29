ae8a93b1f6e3eb3bd21daa016ba45acd6268061e
David Hidalgo <davidhiga@unisabana.edu.co>
2020-05-29 14:26:22 -0500

    alerta de prueba

--- src/app/pages/producto/producto.component.ts
+++ src/app/pages/producto/producto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-producto',
@@ -11,5 +12,13 @@ export class ProductoComponent implements OnInit {
   constructor(private ruta: Router) { }
 
   ngOnInit(): void {
+  }
+  showModal(){
+    Swal.fire(
+      '¡Buen provecho!',
+      'El producto se ha agregado al carrito',
+      'success'
+    )
+
   }
 }
